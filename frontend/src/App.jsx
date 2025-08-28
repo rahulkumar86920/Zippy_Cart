bca87f1a239e39e1fded046863e3a6fb19a943b0
Rahul Kumar <rahulkumar86920@gmail.com>
2025-08-28 14:05:26 +0530

    i have created the folders and have initialized the github

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes } from "react-router" 
+import Navbar from './components/Navbar.jsx'
+
+function App() {
+  return (
+  
+  <Navbar/>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <h1>helli im navbar </h1>
+  )
+}
+
+export default Navbar;
