1ef02efdc12407f3c478fac76ff88f51ca82da66
Rahul Kumar <rahulkumar86920@gmail.com>
2025-08-28 14:19:48 +0530

    added the images of the items in the asset folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Routes } from "react-router" 
+import { Routes } from "react-router"
 import Navbar from './components/Navbar.jsx'
 
 function App() {
   return (
-  
-  <Navbar/>
+    <>
+      <Navbar />
+    </>
+
   )
 }
 
